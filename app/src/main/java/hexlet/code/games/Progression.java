b70cc896349f4b08e957ae7b6ea6c074c3a4fbc7
anastasiaBliznetsova <bliznecovanastasia@gmail.com>
2023-09-05 21:12:45 +0600

    fix magicNumbers3

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Progression {
-    static int valueForRirstNumber = 20;
-    static int valueForStep = 8;
-    static int valueForMissingNumber = 9;
+    private static int valueForRirstNumber = 20;
+    private static int valueForStep = 8;
+    private static int valueForMissingNumber = 9;
     public static Map<String, String> arithmeticProgression() {
         Map<String, String> output = new HashMap<>();
 
