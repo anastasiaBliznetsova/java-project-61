cb1da271ac69e653e925a9d3d0d2d6f9a4d8162f
anastasiaBliznetsova <bliznecovanastasia@gmail.com>
2023-09-07 18:16:26 +0600

    fix linter errors

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -17,9 +17,11 @@ public class Prime {
         Engine.gameEngine(output);
     }
     public static boolean isPrimeNumber(int number) {
-        if(number < 2) return false;
-        for(int i = 2; i < number / 2; i++) {
-            if(number % i == 0) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
